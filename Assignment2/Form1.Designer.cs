ddbf05890e2b73579b453b592728d9024eeac4c8
romelaa <34079912+romelaa@users.noreply.github.com>
2020-02-24 09:53:10 -0700

    Die guessing game files importing

---
+++ Assignment2/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLost = new System.Windows.Forms.Label();
+            this.lblWon = new System.Windows.Forms.Label();
+            this.lblPlayed = new System.Windows.Forms.Label();
+            this.numLost = new System.Windows.Forms.Label();
+            this.numWon = new System.Windows.Forms.Label();
+            this.numPlayed = new System.Windows.Forms.Label();
+            this.enterGuess = new System.Windows.Forms.Label();
+            this.guessBox = new System.Windows.Forms.TextBox();
+            this.dicePB = new System.Windows.Forms.PictureBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.clmnFace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmnFrequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmnPercent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmnNumOfGuess = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.warningLbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dicePB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.lblLost);
+            this.groupBox1.Controls.Add(this.lblWon);
+            this.groupBox1.Controls.Add(this.lblPlayed);
+            this.groupBox1.Controls.Add(this.numLost);
+            this.groupBox1.Controls.Add(this.numWon);
+            this.groupBox1.Controls.Add(this.numPlayed);
+            this.groupBox1.Location = new System.Drawing.Point(154, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 146);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game Info";
+            // 
+            // lblLost
+            // 
+            this.lblLost.AutoSize = true;
+            this.lblLost.Location = new System.Drawing.Point(194, 99);
+            this.lblLost.Name = "lblLost";
+            this.lblLost.Size = new System.Drawing.Size(35, 13);
+            this.lblLost.TabIndex = 5;
+            this.lblLost.Text = "label1";
+            // 
+            // lblWon
+            // 
+            this.lblWon.AutoSize = true;
+            this.lblWon.Location = new System.Drawing.Point(194, 66);
+            this.lblWon.Name = "lblWon";
+            this.lblWon.Size = new System.Drawing.Size(35, 13);
+            this.lblWon.TabIndex = 4;
+            this.lblWon.Text = "label1";
+            // 
+            // lblPlayed
+            // 
+            this.lblPlayed.AutoSize = true;
+            this.lblPlayed.Location = new System.Drawing.Point(194, 32);
+            this.lblPlayed.Name = "lblPlayed";
+            this.lblPlayed.Size = new System.Drawing.Size(35, 13);
+            this.lblPlayed.TabIndex = 3;
+            this.lblPlayed.Text = "label1";
+            // 
+            // numLost
+            // 
+            this.numLost.AutoSize = true;
+            this.numLost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numLost.Location = new System.Drawing.Point(17, 99);
+            this.numLost.Name = "numLost";
+            this.numLost.Size = new System.Drawing.Size(152, 15);
+            this.numLost.TabIndex = 2;
+            this.numLost.Text = "Number of Times Lost:";
+            // 
+            // numWon
+            // 
+            this.numWon.AutoSize = true;
+            this.numWon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numWon.Location = new System.Drawing.Point(17, 66);
+            this.numWon.Name = "numWon";
+            this.numWon.Size = new System.Drawing.Size(153, 15);
+            this.numWon.TabIndex = 1;
+            this.numWon.Text = "Number of Times Won:";
+            // 
+            // numPlayed
+            // 
+            this.numPlayed.AutoSize = true;
+            this.numPlayed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numPlayed.Location = new System.Drawing.Point(17, 32);
+            this.numPlayed.Name = "numPlayed";
+            this.numPlayed.Size = new System.Drawing.Size(168, 15);
+            this.numPlayed.TabIndex = 0;
+            this.numPlayed.Text = "Number of Times Played:";
+            // 
+            // enterGuess
+            // 
+            this.enterGuess.AutoSize = true;
+            this.enterGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterGuess.Location = new System.Drawing.Point(171, 207);
+            this.enterGuess.Name = "enterGuess";
+            this.enterGuess.Size = new System.Drawing.Size(163, 16);
+            this.enterGuess.TabIndex = 1;
+            this.enterGuess.Text = "Enter your guess (1-6):";
+            // 
+            // guessBox
+            // 
+            this.guessBox.Location = new System.Drawing.Point(351, 207);
+            this.guessBox.MaxLength = 1;
+            this.guessBox.Name = "guessBox";
+            this.guessBox.Size = new System.Drawing.Size(48, 20);
+            this.guessBox.TabIndex = 2;
+            // 
+            // dicePB
+            // 
+            this.dicePB.Location = new System.Drawing.Point(174, 246);
+            this.dicePB.Name = "dicePB";
+            this.dicePB.Size = new System.Drawing.Size(100, 104);
+            this.dicePB.TabIndex = 3;
+            this.dicePB.TabStop = false;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(316, 259);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(83, 28);
+            this.btnRoll.TabIndex = 4;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(316, 304);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(83, 28);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.BackgroundColor = System.Drawing.Color.White;
+            this.dataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGrid.ColumnHeadersHeight = 30;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmnFace,
+            this.clmnFrequency,
+            this.clmnPercent,
+            this.clmnNumOfGuess});
+            this.dataGrid.Location = new System.Drawing.Point(73, 411);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.Size = new System.Drawing.Size(416, 173);
+            this.dataGrid.TabIndex = 6;
+            // 
+            // clmnFace
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.clmnFace.DefaultCellStyle = dataGridViewCellStyle1;
+            this.clmnFace.HeaderText = "Face";
+            this.clmnFace.Name = "clmnFace";
+            this.clmnFace.ReadOnly = true;
+            this.clmnFace.Width = 50;
+            // 
+            // clmnFrequency
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.clmnFrequency.DefaultCellStyle = dataGridViewCellStyle2;
+            this.clmnFrequency.HeaderText = "Frequency";
+            this.clmnFrequency.Name = "clmnFrequency";
+            this.clmnFrequency.ReadOnly = true;
+            // 
+            // clmnPercent
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.clmnPercent.DefaultCellStyle = dataGridViewCellStyle3;
+            this.clmnPercent.HeaderText = "Percent";
+            this.clmnPercent.Name = "clmnPercent";
+            this.clmnPercent.ReadOnly = true;
+            this.clmnPercent.Width = 70;
+            // 
+            // clmnNumOfGuess
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.clmnNumOfGuess.DefaultCellStyle = dataGridViewCellStyle4;
+            this.clmnNumOfGuess.HeaderText = "Number of Times Guessed";
+            this.clmnNumOfGuess.Name = "clmnNumOfGuess";
+            this.clmnNumOfGuess.ReadOnly = true;
+            this.clmnNumOfGuess.Width = 150;
+            // 
+            // warningLbl
+            // 
+            this.warningLbl.AutoSize = true;
+            this.warningLbl.ForeColor = System.Drawing.Color.Maroon;
+            this.warningLbl.Location = new System.Drawing.Point(171, 376);
+            this.warningLbl.Name = "warningLbl";
+            this.warningLbl.Size = new System.Drawing.Size(35, 13);
+            this.warningLbl.TabIndex = 7;
+            this.warningLbl.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(570, 626);
+            this.Controls.Add(this.warningLbl);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.dicePB);
+            this.Controls.Add(this.guessBox);
+            this.Controls.Add(this.enterGuess);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Die Guess Game";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dicePB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label numLost;
+        private System.Windows.Forms.Label numWon;
+        private System.Windows.Forms.Label numPlayed;
+        private System.Windows.Forms.Label enterGuess;
+        private System.Windows.Forms.TextBox guessBox;
+        private System.Windows.Forms.PictureBox dicePB;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmnFace;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmnFrequency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmnPercent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmnNumOfGuess;
+        private System.Windows.Forms.Label warningLbl;
+        private System.Windows.Forms.Label lblLost;
+        private System.Windows.Forms.Label lblWon;
+        private System.Windows.Forms.Label lblPlayed;
+    }
+}
+
---
+++ Assignment2/Form1.cs
@@ -0,0 +1,273 @@
+﻿/// <sumary>
+/// Romela Azizyan
+/// CS 3280 - Assignment 2 Dice Roll
+/// 
+/// This program takes a user input and checks to see if the dice rolled matches to the input value.
+/// 
+/// </sumary>
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+        #region Attributes
+        /// <summary>
+        ///  the number that the user entered in the textbox
+        /// </summary>
+        private int guess = 0;
+
+        /// <summary>
+        /// tracks number of times lost
+        /// </summary>
+        private int timesLost = 0;
+        /// <summary>
+        /// tracks number of times played
+        /// </summary>
+        private int timesPlayed = 0;
+
+        /// <summary>
+        /// tracks number of times won
+        /// </summary>
+        private int timesWon = 0;
+
+        /// <summary>
+        /// number of times user chose 1
+        /// </summary>
+        private int userchoice1 = 0;
+
+        /// <summary>
+        /// number of times user chose 2
+        /// </summary>
+        private int userchoice2 = 0;
+
+        /// <summary>
+        /// number of times user chose 3
+        /// </summary>
+        private int userchoice3 = 0;
+
+        /// <summary>
+        /// number of times user chose 4
+        /// </summary>
+        private int userchoice4 = 0;
+
+        /// <summary>
+        /// number of times user chose 5
+        /// </summary>
+        private int userchoice5 = 0;
+
+        /// <summary>
+        /// number of times user chose 6
+        /// </summary>
+        private int userchoice6 = 0;
+        
+        /// <summary>
+        /// stores the random number of the die
+        /// </summary>
+        private int randomdie = 0;
+
+        /// <summary>
+        /// number of times the correct number was 1
+        /// </summary>
+        private int countFace1 = 0;
+
+        /// <summary>
+        /// number of times the correct number was 2
+        /// </summary>
+        private int countFace2 = 0;
+
+        /// <summary>
+        /// number of times the correct number was 3
+        /// </summary>
+        private int countFace3 = 0;
+
+        /// <summary>
+        /// number of times the correct number was 4
+        /// </summary>
+        private int countFace4 = 0;
+
+        /// <summary>
+        /// number of times the correct number was 5
+        /// </summary>
+        private int countFace5 = 0;
+
+        /// <summary>
+        /// number of times the correct number was 6
+        /// </summary>
+        private int countFace6 = 0;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGrid.Rows.Add(6); //six rows in the data grid of the form
+            dataGrid.Columns[2].DefaultCellStyle.Format = "0.00,%";
+            lblLost.Text = "0";
+            lblPlayed.Text = "0";
+            lblWon.Text = "0";
+            warningLbl.Visible = false;
+        }
+
+        /// <summary>
+        /// when the button Roll is clicked the game starts
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            dicePB.SizeMode = PictureBoxSizeMode.StretchImage;
+            Random randomNum = new Random();
+            Int32.TryParse(guessBox.Text, out guess);
+            warningLbl.Visible = false;
+            
+            if (guess < 1 || guess > 6) // if the guess is less than 1 or greater than 6
+            {
+                warningLbl.Text = "Invalid input. You must enter a number between 1 through 6";
+                warningLbl.Visible = true;
+                warningLbl.Refresh();
+            }
+            else if (string.IsNullOrWhiteSpace(guessBox.Text)) // if no value was entered in the textbox
+            {
+                warningLbl.Text = "Invalid input. You must enter a number between 1 through 6";
+                warningLbl.Visible = true;
+                warningLbl.Refresh();
+            }
+
+            else { // if the guess is in between 1 and 6 check if the number matches or doesn't
+
+                for (int i = 1; i < 10; i++) // cycles through the random numbers 9 times
+                    {
+                        randomdie = randomNum.Next(1, 7);
+                        warningLbl.Visible = true;
+                        // randomly choosing an image
+                        dicePB.Image = Image.FromFile("die" + randomdie.ToString() + ".gif");
+                        // refreshing the image
+                        dicePB.Refresh();
+                        // sleep thread - slows down the pace of changing images for the illusion of a dice roll
+                        Thread.Sleep(200);
+
+                    if (i == 9) // when the 9th random number is picked check if the guess is correct or not
+                    {
+                        // keeps track of the count user chose one of the 6 faces
+                        // these numbers are later stored in an array to display the number of times guessed column
+                        if (guess == 1)
+                        {
+                            userchoice1++;
+                        }
+                        else if (guess == 2)
+                        {
+                            userchoice2++;
+                        }
+                        else if (guess == 3)
+                        {
+                            userchoice3++;
+                        }
+                        else if (guess == 4)
+                        {
+                            userchoice4++;
+                        }
+                        else if (guess == 5)
+                        {
+                            userchoice5++;
+                        }
+                        else if (guess == 6) {
+                            userchoice6++;
+                        }
+                        
+                        // if the guess matches the number then number of times won and the number of times played increment by one
+                        if (guess == randomdie)
+                        {
+                            timesWon++;
+                            lblWon.Text = timesWon.ToString();
+                            timesPlayed++;
+                            lblPlayed.Text = timesPlayed.ToString();
+                            warningLbl.Visible = true;
+                            warningLbl.Text = "Congratulations you guessed the number " + randomdie + ".";
+                        }
+                        else //if the guess does not match then the number of times lost and the number of times played increment by one
+                        {
+                            timesLost++;
+                            lblLost.Text = timesLost.ToString();
+                            timesPlayed++;
+                            lblPlayed.Text = timesPlayed.ToString();
+                            warningLbl.Visible = true;
+                            warningLbl.Text = "You did not guess this time, try again.";
+                        }
+                        // adds 1 to count each time one of the cases is "true" (meaning one of the cases is the correct answer)
+                        // these numbers are later stored in an array to display the frequencies
+                        if (randomdie == 1) {
+                            countFace1++;
+                        }
+                        else if (randomdie == 2)
+                        {
+                            countFace2++;
+                        }
+                        else if (randomdie == 3)
+                        {
+                            countFace3++;
+                        }
+                        else if (randomdie == 4)
+                        {
+                            countFace4++;
+                        }
+                        else if (randomdie == 5)
+                        {
+                            countFace5++;
+                        }
+                        else if (randomdie == 6)
+                        {
+                            countFace6++;
+                        }
+                    }
+
+                }
+
+                // sets the row values accordingly
+                // referenced https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datagridview.rows?view=netframework-4.8
+                for (int i = 0; i < 6; i++)
+                {
+                    int[] countfaces = { countFace1, countFace2, countFace3, countFace4, countFace5, countFace6 };
+                    int[] userGuesses = { userchoice1, userchoice2, userchoice3, userchoice4, userchoice5, userchoice6 };
+                    dataGrid.Rows[i].Cells[0].Value = i+1; // lists 1-6 in the face column
+                    dataGrid.Rows[i].Cells[1].Value = countfaces[i]; // lists the number of times the die was rolled on a number
+                    dataGrid.Rows[i].Cells[2].Value = (float)(countfaces[i] / (float)timesPlayed); // Counts the percentage
+                    dataGrid.Rows[i].Cells[3].Value = userGuesses[i]; // lists the number of times user chose the specific number
+                }
+
+                dataGrid.Refresh();
+
+            }
+        }
+        
+        /// <summary>
+        /// when the button Reset is clicked the form resets
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //resets the number of times played, won, frequencies, and number of times guessed
+            // dataGrid.Rows.Clear(); // this actually clears all the data from the data grid so the reset button doesn't work properly
+            // this.dataGrid.Refresh();
+            timesLost = 0;
+            lblLost.Text = timesLost.ToString();
+            timesPlayed = 0;
+            lblPlayed.Text = timesPlayed.ToString();
+            timesWon = 0;
+            lblWon.Text = timesWon.ToString();
+           
+
+        }
+    }
+}
